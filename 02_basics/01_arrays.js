d48f7ac5a89d4eb35530cf9e2cb67ebae113be3d
Ayushi <65006070+ayushijha0920@users.noreply.github.com>
2025-09-07 07:16:57 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+// //array
+
+const myArray = [1, 2, 3, 4, true, "Ayushi"] //elements can be string, number or boolean
+
+// console.log(myArray[0]) // we can access an array giving its index 
+
+// // Shallow copies 
+// //Deep Copies
+
+// // another way to declare aarays
+// const myArray2 = new Array(1, 2, 3, 4, 5)
+
+// // Methods of Array 
+// myArray.push(6) 
+// myArray.push(7) //to add another value in the array
+// myArray.pop()  // to remove last value from array
+// myArray.unshift(0) // to add a value in array in the start or 0 index
+// myArray.shift() // to remove value from thhe start
+
+// console.log(myArray.includes(9)) // comes the output in boolean
+// console.log(myArray.indexOf(3));
+
+// const newArr = myArray.join() // .join adds all the elements of an array into string
+
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+
+
+// slice, splice
+
+console.log("A", myArray);
+
+
+const myn1 =  myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B", myArray
+      
+);
+
+
