838bcc06f2e535e5480a6488dc82d6016d26c93f
Ayushi <65006070+ayushijha0920@users.noreply.github.com>
2025-09-14 16:43:55 +0000

    objects

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,41 +1,44 @@
-const marvel_heros = ["thor", "ironman","spiderman"]
-const dc = ["superman", "flash", "batman"]
+// const marvel_heros = ["thor", "ironman","spiderman"]
+// const dc = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc)
+// // marvel_heros.push(dc)
 
-// console.log(marvel_heros)
-// console.log(marvel_heros[3][1])
+// // console.log(marvel_heros)
+// // console.log(marvel_heros[3][1])
 
-const all_heros = marvel_heros.concat(dc)
-// console.log(all_heros)
-
-
-const all_new_heros = [...marvel_heros, ...dc]   //spread
-// console.log(all_new_heros);
+// const all_heros = marvel_heros.concat(dc)
+// // console.log(all_heros)
 
 
-const another_array = [1,2,3,[4,5,[6,7,8]]]
-
-const another_real_array = another_array.flat(Infinity)
-
-console.log(another_real_array)
+// const all_new_heros = [...marvel_heros, ...dc]   //spread
+// // console.log(all_new_heros);
 
 
+// const another_array = [1,2,3,[4,5,[6,7,8]]]
 
-console.log(Array.isArray("Ayushi"
+// const another_real_array = another_array.flat(Infinity)
 
-));
-
-console.log(Array.from("Ayushi"));
-
-
-console.log(Array.from({name:"Ayushi"}));//Interesting
-
-let score1 = 100
-let score2 = 200
-let score3 = 300
-
-console.log(Array.of(score1, score2, score3));
+// console.log(another_real_array)
 
 
 
+// console.log(Array.isArray("Ayushi"
+
+// ));
+
+// console.log(Array.from("Ayushi"));
+
+
+// console.log(Array.from({name:"Ayushi"}));//Interesting
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1, score2, score3));
+
+
+
+const  arr1 = ["A", "B", "C", "D", "E","F", "G"]
+
+const arr2 = ["H", "I", "J", 1, 2, 3, 4, 5]
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,31 @@
+//ObjectsThere are two ways to declare an object
+// 1. literals
+// 2. Constructors
+
+
+
+ // Singleton or constructor
+ 
+ Object.create
+
+
+ //Objects Literals
+
+ const JsUser = {
+
+    name : "Ayushi",
+    "full name": "Ayushi Jha",
+    age: 26,
+    email:"ayushi@google.com",
+    location: "Delhi",
+    isLoggedIn: false,
+    lastLoggIn: ["Monday"]
+ }
+
+ 
+
+console.log(JsUser.email);
+
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+
